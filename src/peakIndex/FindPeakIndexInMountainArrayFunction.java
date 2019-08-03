adf7cda629ee4655b95581d938f321889423dc88
chaninkou <michaelkoulove@yahoo.com>
2019-08-02 23:27:43 -0700

    Able to find the peak of the array. Not that important.

---
+++ src/peakIndex/FindPeakIndexInMountainArrayFunction.java
@@ -0,0 +1,13 @@
+package peakIndex;
+
+public class FindPeakIndexInMountainArrayFunction {
+    public int peakIndexInMountainArray(int[] A) {
+        for(int i = 1; i + 1 < A.length; i++){
+            if(A[i] > A[i + 1]){
+                return i;
+            }
+        }
+        
+        return 0;
+    }
+}
---
+++ src/peakIndex/Main.java
@@ -0,0 +1,16 @@
+package peakIndex;
+
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] A = {0,1,2,1,0};
+		
+		System.out.println("Input: " + Arrays.toString(A));
+		
+		FindPeakIndexInMountainArrayFunction solution = new FindPeakIndexInMountainArrayFunction();
+		
+		System.out.println("Solution: " + solution.peakIndexInMountainArray(A));
+		
+	}
+}
